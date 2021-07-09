9fbc7bb2f059214f085b7de675cc59faebb84e4f
JAYA CHANDRA <thottempudi56@gmail.com>
2021-07-09 17:22:48 +0530

    check2

--- app.js
+++ app.js
@@ -95,4 +95,4 @@ const server = http.createServer((req, res) => {
 })
 
 
-server.listen(5000, '0.0.0.0' , () => console.log("Server running on port 5000..."))
+server.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000..."))
